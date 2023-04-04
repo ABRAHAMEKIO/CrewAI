be634ffce66f47bbaf5774a6cf12641209adc7fd
empeje <11813607+empeje@users.noreply.github.com>
2023-04-05 01:05:08 +0200

    Initial layout

--- packages/crew/pages/_app.tsx
+++ packages/crew/pages/_app.tsx
@@ -1,16 +1,48 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import {createTheme, NextUIProvider} from "@nextui-org/react";
+
+const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: '$green200',
+      primaryLightHover: '$green300',
+      primaryLightActive: '$green400',
+      primaryLightContrast: '$green600',
+      primary: '#4ADE7B',
+      primaryBorder: '$green500',
+      primaryBorderHover: '$green600',
+      primarySolidHover: '$green700',
+      primarySolidContrast: '$white',
+      primaryShadow: '$green500',
+
+      gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      link: '#5E1DAD',
+
+      // you can also create your own color
+      myColor: '#ff4ecd'
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {}
+  }
+})
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextUIProvider theme={theme}>
       <Head>
         <title>Welcome to crew!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
+      </NextUIProvider>
     </>
   );
 }
