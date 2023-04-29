29fc15c7c144b5c5662b3245d11ea9289204a864
Rahma Andita Purnama <purwantiibuku@gmail.com>
2023-04-29 09:32:05 +0700

    delete var database_url from config.ts

--- packages/crew/config.ts
+++ packages/crew/config.ts
@@ -7,4 +7,3 @@ export const awsId = process.env.AWS_ACCESS_KEY_ID;
 export const awsSecret = process.env.AWS_SECRET_ACCESS_KEY;
 export const awsBucket = process.env.AWS_BUCKET;
 export const awsRegion = process.env.AWS_REGION;
-export const databaseUrl = process.env.DATABASE_URL;
