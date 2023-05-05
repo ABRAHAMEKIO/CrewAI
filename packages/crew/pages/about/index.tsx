43823aa38756d733e435e25037b6568ba8195b11
Rahma Andita Purnama <purwantiibuku@gmail.com>
2023-05-05 15:15:27 +0700

    improve ui

--- packages/crew/pages/about/index.tsx
+++ packages/crew/pages/about/index.tsx
@@ -1,54 +1,122 @@
 import React from 'react';
-import { Container } from '@nextui-org/react';
+import { Button, Container, Grid, Spacer, Text } from '@nextui-org/react';
 import Image from 'next/image';
 import Layout from '../../components/Layout';
 import NavigationBar from '../../components/NavigationBar';
-import { Header1, Header2 } from '../../components/Heading';
 
 function Index() {
+  const teams = [
+    {
+      name: 'mpj',
+      image: 'https://github.com/empeje.png',
+      description: `mpj is the lead developer at crew, he's long time hacker and developer and loves to read.`,
+    },
+    {
+      name: 'huda',
+      image: 'https://github.com/hudakurniawan.png',
+      description: `huda is the product manager at crew, he's long time business developer and iot engineer.`,
+    },
+  ];
   return (
     <Layout>
       <NavigationBar />
-      <Container>
-        <Header1 content="About us" />
-        <p>
-          CrewAI is crew of ambitious individuals exploring what&apos;s possible
-          with the intersection of GenerativeAI and Web3.
-        </p>
-        <p>
-          Our goal is to provide you with the best Generative AI utilities to
-          express yourself in the new world where AI assist people to be more
-          creative.
-        </p>
-        <Header2 content="Team" />
-        <p>
-          <strong>mpj</strong>
-        </p>
-        <Image
-          src="https://github.com/empeje.png"
-          width={400}
-          height={400}
-          alt="mpj's avatar"
-        />
-        <p>
-          <em>mpj</em> is the lead developer at crew, he&apos;s long time hacker
-          and developer and loves to read.
-        </p>
-
-        <p>
-          <strong>huda</strong>
-        </p>
-        <Image
-          src="https://github.com/hudakurniawan.png"
-          width={400}
-          height={400}
-          alt="huda's avatar"
-        />
-        <p>
-          <em>huda</em> is the product manager at crew, he&apos;s long time
-          business developer and iot engineer.
-        </p>
-      </Container>
+      <div
+        style={{
+          paddingTop: '6rem',
+        }}
+      >
+        <Container>
+          <Grid.Container justify="center" gap={6}>
+            <Grid xs={12} sm={5} direction="column" css={{ p: 0 }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Text
+                  h1
+                  size={60}
+                  css={{
+                    textGradient: '45deg, $blue600 -20%, $pink600 50%',
+                  }}
+                  weight="bold"
+                >
+                  About Us
+                </Text>
+              </div>
+              <Text
+                size={24}
+                css={{
+                  textAlign: 'center',
+                }}
+              >
+                CrewAI is crew of ambitious individuals exploring what&apos;s
+                possible with the intersection of GenerativeAI and Web3.
+              </Text>
+              <Text
+                size={24}
+                css={{
+                  textAlign: 'center',
+                }}
+              >
+                Our goal is to provide you with the best Generative AI utilities
+                to express yourself in the new world where AI assist people to
+                be more creative.
+              </Text>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  marginTop: '3rem',
+                }}
+              >
+                <Text
+                  h1
+                  size={42}
+                  css={{
+                    textGradient: '45deg, $yellow600 -20%, $red600 100%',
+                    alignItems: 'center',
+                  }}
+                  weight="bold"
+                >
+                  Meet the Team
+                </Text>
+                <Spacer y={1.5} />
+              </div>
+            </Grid>
+          </Grid.Container>
+        </Container>
+        <Container sm>
+          <Grid.Container justify="center" gap={2}>
+            {teams.map((team) => {
+              return (
+                <Grid justify="center" xs={12} sm={6}>
+                  <div
+                    style={{
+                      alignItems: 'center',
+                      maxWidth: '300px',
+                    }}
+                  >
+                    <Text size={28}>
+                      <strong>{team.name}</strong>
+                    </Text>
+                    <Image
+                      src={team.image}
+                      width={300}
+                      height={300}
+                      alt={`${team.name} s avatar`}
+                    />
+                    <Text size={24}>{team.description}</Text>
+                  </div>
+                </Grid>
+              );
+            })}
+          </Grid.Container>
+        </Container>
+      </div>
     </Layout>
   );
 }
