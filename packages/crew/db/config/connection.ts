97319a7428beabbfd60a95b84eb2f75732f36f74
empeje <11813607+empeje@users.noreply.github.com>
2023-05-07 02:44:58 +0200

    Try to fix one more time

--- packages/crew/db/config/connection.ts
+++ packages/crew/db/config/connection.ts
@@ -1,19 +1,23 @@
 import { Sequelize } from 'sequelize';
 
-const sequelizeConnection = new Sequelize(process.env.NX_DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelizeConnection = new Sequelize(
+  process.env.NX_DATABASE_URL ||
+    'postgres://postgres:postgres@localhost:5432/crew',
+  {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 export default sequelizeConnection;
