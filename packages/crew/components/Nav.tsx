8c1adfb895bd6b1968f84710faf950f5d2e9fea4
yuliefas <yuliefas@gmail.com>
2023-05-09 20:37:23 +0700

    Toggle feature nav

--- packages/crew/components/Nav.tsx
+++ packages/crew/components/Nav.tsx
@@ -16,6 +16,8 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
+const showFeature = false;
+
 function Nav({ className }: { className?: string }) {
   return (
     <Disclosure as="nav" className={className || 'bg-white border-b'}>
@@ -29,72 +31,78 @@ function Nav({ className }: { className?: string }) {
                   <span className="sr-only">Open main menu</span>
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-between sm:items-stretch">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch">
                 <div className="flex flex-shrink-0 items-center space-x-4">
                   <h1 className="font-bold text-base sm:text-2xl not-italic">
                     Hologram
                   </h1>
                 </div>
-                <div className="hidden sm:ml-6 sm:flex items-center w-full justify-center">
-                  <div className="flex space-x-[2.5rem] justify-center">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="font-normal text-base"
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
+                {showFeature && (
+                  <div className="sm:ml-6 flex items-center w-full justify-center">
+                    <div className="flex space-x-[2.5rem] justify-center">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="font-normal text-base"
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
 
-              <div className="lex items-center sm:static sm:inset-auto w-fit">
-                <div className="flex space-x-4">
-                  {nivigationRight.map((item) => {
-                    return (
-                      <Disclosure.Button
-                        key="connect-wallet"
-                        className={classNames(
-                          item.bgDark
-                            ? 'bg-gray-900 text-white'
-                            : 'bg-white text-black ',
-                          'border rounded-lg px-[1.5rem] text-sm font-medium h-[2.5rem] sm:h-12 min-w-[117px]'
-                        )}
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    );
-                  })}
-                </div>
+              {showFeature && (
+                <div className="lex items-center sm:static sm:inset-auto w-fit">
+                  <div className="flex space-x-4">
+                    {nivigationRight.map((item) => {
+                      return (
+                        <Disclosure.Button
+                          key="connect-wallet"
+                          className={classNames(
+                            item.bgDark
+                              ? 'bg-gray-900 text-white'
+                              : 'bg-white text-black ',
+                            'border rounded-lg px-[1.5rem] text-sm font-medium h-[2.5rem] sm:h-12 min-w-[117px]'
+                          )}
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      );
+                    })}
+                  </div>
 
-                {/* Profile dropdown */}
-              </div>
+                  {/* Profile dropdown */}
+                </div>
+              )}
             </div>
           </Section>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
+          {showFeature && (
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          )}
         </>
       )}
     </Disclosure>
