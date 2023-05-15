84890e58de2912a0e4b508f3648fcd1ad94e9332
empeje <11813607+empeje@users.noreply.github.com>
2023-05-15 14:14:51 +0200

    Fix bug related to prompt

--- packages/crew/pages/api/openjourney/index.ts
+++ packages/crew/pages/api/openjourney/index.ts
@@ -16,7 +16,7 @@ export default async (req, res) => {
     const prediction = await replicate.predictions.create({
       version:
         '9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb',
-      input: { prompt: prompt.prompt },
+      input: { prompt: msg },
       webhook: WEBHOOK_OVERRIDE,
       webhook_events_filter: ['completed'],
     });
