13b2ded17736bfa9bf4f7e760b83e97f365b1f52
empeje <11813607+empeje@users.noreply.github.com>
2023-05-16 15:58:53 +0200

    Add sentry

--- packages/crew/next.config.js
+++ packages/crew/next.config.js
@@ -3,6 +3,24 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  org: 'hologram-ai',
+  project: 'javascript-nextjs',
+
+  silent: true, // Suppresses all logs
+
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  * */
@@ -25,4 +43,9 @@ const nextConfig = {
   },
 };
 
-module.exports = withNx(nextConfig);
+module.exports = async (phase) => {
+  const nxConfig = await withNx(nextConfig)(phase);
+  const config = withSentryConfig(nxConfig, sentryWebpackPluginOptions);
+
+  return config;
+};
---
+++ packages/crew/sentry.client.config.ts
@@ -0,0 +1,23 @@
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://d5fefaadd7364c43a09e07801955249a@o4505193919676416.ingest.sentry.io/4505193922297856',
+  // Replay may only be enabled for the client-side
+  integrations: [new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+
+  // ...
+
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
