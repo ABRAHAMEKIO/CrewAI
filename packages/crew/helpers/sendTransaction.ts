d5bc8649b945243ed2146bd0321fdfaca9bb4adb
Ahluzain Octuviadi <ahloct11@gmail.com>
2023-05-19 17:34:13 +0700

    condition kulkul address (gnosis or polygon)

--- packages/crew/helpers/sendTransaction.ts
+++ packages/crew/helpers/sendTransaction.ts
@@ -4,8 +4,13 @@ async function sendTransaction(sendTokenAmount: string) {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
+    const network = (await provider.getNetwork()).name;
+
     const tx = await signer.sendTransaction({
-      to: '0xd54E6A61332657eCac42146f226e44C6166C86bE',
+      to:
+        network === 'matic'
+          ? '0x2ab35CA8EFEbD8663B709160ACAcb160692dBfB1'
+          : '0xd54E6A61332657eCac42146f226e44C6166C86bE',
       value: ethers.utils.parseUnits(sendTokenAmount, 'ether').toHexString(),
     });
 
