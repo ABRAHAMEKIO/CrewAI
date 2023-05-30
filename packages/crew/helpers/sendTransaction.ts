3d33933fb5c82d627e65ebfba03f69d66c88f695
Ahluzain Octuviadi <ahloct11@gmail.com>
2023-05-30 14:45:02 +0700

    define chain ID after sendTransaction

--- packages/crew/helpers/sendTransaction.ts
+++ packages/crew/helpers/sendTransaction.ts
@@ -5,15 +5,16 @@ async function sendTransaction(sendTokenAmount: string) {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const network = (await provider.getNetwork()).name;
+    const network = await provider.getNetwork();
 
     const tx = await signer.sendTransaction({
-      to: network === 'matic' ? web3AddressPolygon : web3AddressGnosis,
+      to: network.chainId === 100 ? web3AddressGnosis : web3AddressPolygon,
       value: ethers.utils.parseUnits(sendTokenAmount, 'ether').toHexString(),
     });
 
     const rc = await tx.wait();
     if (rc.status === 1) {
+      tx.chainId = network.chainId;
       return tx;
     }
 
